23800347da62076a62e9ac18780988ea3f031623
sunninz <tnals00718@gmail.com>
2025-05-22 13:37:27 +0900

    :sparkles: (#3) @RestController로 어노테이션 변경

--- src/main/java/com/example/SpringStudy/web/controller/MemberController.java
+++ src/main/java/com/example/SpringStudy/web/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
