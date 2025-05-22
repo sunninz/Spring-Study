c8e90fce9f3c78cec22c62dbf5c973d415ed170b
sunninz <tnals00718@gmail.com>
2025-05-22 14:12:33 +0900

    :sparkles: (#3) Member 엔티티에 @DynamicInsert/@DynamicUpdate 적용

--- src/main/java/com/example/SpringStudy/domain/Member.java
+++ src/main/java/com/example/SpringStudy/domain/Member.java
@@ -9,6 +9,9 @@ import com.example.SpringStudy.domain.mapping.MemberPrefer;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -16,6 +19,8 @@ import java.util.List;
 
 @Entity
 @Getter
+@DynamicInsert
+@DynamicUpdate
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -40,6 +45,7 @@ public class Member extends BaseEntity {
     @Column(nullable = false, length = 40)
     private String specAddress;
 
+    @ColumnDefault("0")
     private Integer point;
 
     private LocalDate birth;
