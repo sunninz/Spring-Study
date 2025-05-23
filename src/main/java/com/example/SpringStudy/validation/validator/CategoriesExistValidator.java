85d2198164696880dcaf962b74b812ab5951b004
sunninz <tnals00718@gmail.com>
2025-05-23 10:06:47 +0900

    :sparkles: (#3) 어노테이션을 활용한 validation

---
+++ src/main/java/com/example/SpringStudy/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,37 @@
+package com.example.SpringStudy.validation.validator;
+
+import com.example.SpringStudy.apiPayload.code.status.ErrorStatus;
+import com.example.SpringStudy.repository.FoodCategoryRepository.FoodCategoryRepository;
+import com.example.SpringStudy.validation.annotation.ExistCategories;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator <ExistCategories, List<Long>> {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+
+        boolean isValid = values.stream()
+                .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        if (!isValid){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+
+}
--- src/main/java/com/example/SpringStudy/web/dto/request/MemberRequestDTO.java
+++ src/main/java/com/example/SpringStudy/web/dto/request/MemberRequestDTO.java
@@ -1,5 +1,9 @@
 package com.example.SpringStudy.web.dto.request;
 
+import com.example.SpringStudy.validation.annotation.ExistCategories;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.util.List;
@@ -8,14 +12,23 @@ public class MemberRequestDTO {
 
     @Getter
     public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @NotBlank
         String phoneNumber;
+        @ExistCategories
         List<Long> preferCategory;
     }
 
