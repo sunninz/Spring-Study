5546f460901bdd5d1d841bb7cc42baf77f38d25b
sunninz <tnals00718@gmail.com>
2025-05-26 16:04:17 +0900

    :sparkles: (#17) 특정회원의 진행중/완료된 미션 조회

--- src/main/java/com/example/SpringStudy/converter/MemberMissionConverter.java
+++ src/main/java/com/example/SpringStudy/converter/MemberMissionConverter.java
@@ -2,13 +2,17 @@ package com.example.SpringStudy.converter;
 
 import com.example.SpringStudy.domain.Member;
 import com.example.SpringStudy.domain.Mission;
+import com.example.SpringStudy.domain.Store;
 import com.example.SpringStudy.domain.enums.MissionStatus;
 import com.example.SpringStudy.domain.mapping.MemberMission;
 import com.example.SpringStudy.web.dto.response.MemberMissionResponseDTO;
 import com.example.SpringStudy.web.dto.response.MissionResponseDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MemberMissionConverter {
@@ -25,6 +29,30 @@ public class MemberMissionConverter {
                 .member(member)
                 .mission(mission)
                 .status(MissionStatus.CHALLENGING)
+                .build();
+    }
+
+    public static MemberMissionResponseDTO.MissionInfoDTO missionInfoDTO(Mission mission, Store store){
+        return MemberMissionResponseDTO.MissionInfoDTO.builder()
+                .storeName(store.getName())
+                .missionDescription(mission.getMissionDescription())
+                .reward(mission.getReward())
+                .deadline(mission.getDeadline())
+                .build();
+    }
+
+    public static MemberMissionResponseDTO.MissionInfoListDTO missionInfoListDTO(Page<MemberMission> memberMissionList){
+        List<MemberMissionResponseDTO.MissionInfoDTO> missionInfoDTOList = memberMissionList.stream()
+                .map(mm -> MemberMissionConverter.missionInfoDTO(mm.getMission(), mm.getMission().getStore()))
+                .collect(Collectors.toList());
+
+        return MemberMissionResponseDTO.MissionInfoListDTO.builder()
+                .missionInfoDTOList(missionInfoDTOList)
+                .listSize(missionInfoDTOList.size())
+                .totalPage(memberMissionList.getTotalPages())
+                .totalElements(memberMissionList.getTotalElements())
+                .isFirst(memberMissionList.isFirst())
+                .isLast(memberMissionList.isLast())
                 .build();
     }
 }
--- src/main/java/com/example/SpringStudy/service/MemberMissionService/MemberMissionCommandService.java
+++ src/main/java/com/example/SpringStudy/service/MemberMissionService/MemberMissionCommandService.java
@@ -1,9 +1,12 @@
 package com.example.SpringStudy.service.MemberMissionService;
 
+import com.example.SpringStudy.domain.Mission;
+import com.example.SpringStudy.domain.enums.MissionStatus;
 import com.example.SpringStudy.domain.mapping.MemberMission;
 import com.example.SpringStudy.web.dto.request.MemberMissionRequestDTO;
 import com.example.SpringStudy.web.dto.request.ReviewRequestDTO;
 import com.example.SpringStudy.web.dto.response.MemberMissionResponseDTO;
+import org.springframework.data.domain.Page;
 
 public interface MemberMissionCommandService {
     // 미션 등록
@@ -12,4 +15,6 @@ public interface MemberMissionCommandService {
     boolean challengeExist(MemberMissionRequestDTO.ChallengeDTO request);
     // 완료된 미션 검증
     boolean completedMission(ReviewRequestDTO.CreateDTO dto);
+    // 내가 진행중인 미션 목록 조회
+    Page<MemberMission> getMissionsByMemberAndStatus(Long memberId, Integer page, MissionStatus status);
 }
